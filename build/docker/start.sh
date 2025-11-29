9e51bb8a740d6ffeefd9c351fc33681e26a632ec
Edward Heaver <edward.heaver01@gmail.com>
2025-11-29 15:32:50 +0000

    make sure crond is running + set up dockerfile to be simpler to work with

---
+++ build/docker/start.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -euo
+
+log() {
+    level="$1"
+    shift
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [$level] $*"
+}
+
+check_crond_up() {
+    if ! pgrep crond >/dev/null 2>&1; then
+        log ERROR "crond is not running"
+        exit 1
+    fi
+
+    log INFO "crond is running"
+}
+
+log INFO "starting crond in the background"
+crond
+check_crond_up
+
+if [ "$#" -gt 0 ]; then
+    exec "$@"
+fi
--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.24.10
 
 require golang.org/x/text v0.31.0
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require github.com/go-chi/chi/v5 v5.2.3
